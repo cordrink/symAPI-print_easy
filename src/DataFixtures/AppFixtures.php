7c27c1277a91a671c5b55ff3375c07fb659bf211
cordrink <masodavictorjudicael@gmail.com>
2025-04-16 03:43:58 +0100

    feat: Ajout de fausse donnees

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,42 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Customer;
+use App\Entity\Document;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Random\RandomException;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * @throws RandomException
+     */
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+
+        for ($i = 0; $i < 30; $i++) {
+            $customer = new Customer();
+            $customer->setFirstName($faker->firstName)
+                ->setLastName($faker->lastName)
+                ->setCompany($faker->company)
+            ;
+
+            $manager->persist($customer);
+
+            for ($j = 0; $j < random_int(3, 10); $j++) {
+                $document = new Document();
+                $document->setFileName($faker->imageUrl($width = 640, $height = 480))
+                    ->setUploadedAt(new \DateTimeImmutable())
+                    ->setStatus($faker->randomElement(['attente', 'imprime']))
+                    ->setUser($customer)
+                    ;
+
+                $manager->persist($document);
+            }
+        }
 
         $manager->flush();
     }
